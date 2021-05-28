301e6309fda8014f9e8f5eba3e6ccc9de422d9fb
chi793 <71268033+chi793@users.noreply.github.com>
2021-05-28 21:01:15 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,26 @@
+class Launcher{
+    constructor(bodyA,pointB){
+    var options = {
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness:0.01,
+        length:10
+    }
+    this.pointB=pointB;
+   this.launcher=Constraint.create(options);
+    World.add(world,this.launcher);
+    }
+    
+    fly(){
+        this.launcher.bodyA=null;
+    }
+
+   launch(bodyA){
+       this.launcher.bodyA=bodyA;
+   } 
+   display(){
+
+   }
+
+
+}
